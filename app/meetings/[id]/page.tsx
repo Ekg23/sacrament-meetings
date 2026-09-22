9b56186fcc46e0eb345a67dd0fd70667b2fff3e3
Geoffrey Kofi Etu <geoffreyetu56@gmail.com>
2026-09-22 22:26:44 +0000

    Added a function to handle the fetching from the api route meeting by id.

--- app/meetings/[id]/page.tsx
+++ app/meetings/[id]/page.tsx
@@ -2,14 +2,22 @@ import { notFound } from 'next/navigation'
 import MeetingDetail from '@/components/MeetingDetail'
 import { SacramentMeeting } from '@/lib/types'
 
+function getBaseUrl() {
+    if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`;
+    }
+    return 'http://localhost:3000';
+}
+
 export default async function MeetingPage({
     params,
 }: {
     params: Promise<{ id: string }>
 }) {
     const {id} = await params
-    const response = await fetch(
-        `http://localhost:3000/api/meetings/${id}`)
+    const baseUrl = getBaseUrl();
+    const response = await fetch(`${baseUrl}/api/meetings/${id}`, { cache: 'no-store' })
+
 
     if (!response.ok) {
         notFound()
